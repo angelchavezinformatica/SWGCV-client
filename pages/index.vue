8c25d162c0a20137a5343a49522dcddc6df4251d
angelchavezinformatica <achavezg@unitru.edu.pe>
2024-06-18 20:01:06 -0500

    page- Index- Image credits & change slogan

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,16 @@
     <div class="Header-content">
       <h1>Vivero La Huerta</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem
-        ut ex eos voluptate culpa ipsa sunt, porro autem non!
+        Producción y comercialización de hortalizas orgánicas, aplicando buenas
+        prácticas agrícolas.
+      </p>
+    </div>
+    <div class="Header-credits">
+      <p>
+        Créditos:
+        <a href="https://www.facebook.com/vivero.lahuerta.31">
+          Vivero La Huerta
+        </a>
       </p>
     </div>
   </header>
@@ -18,6 +26,7 @@
 <style scope lang="sass">
 .Header
   @include flex-center-center()
+  position: relative
   background-color: $bg-color
   background-image: url('/background.jpeg')
   background-repeat: no-repeat
@@ -31,13 +40,25 @@
     background-color: $bg-color-2
     border-radius: 20px
     width: calc( 100% - 2rem )
-    max-width: 700px
+    max-width: 550px
     padding: .7rem .5rem
-    gap: .7rem
     h1,
     p
       color: $bg-color
       text-align: center
+    h1
+      margin-bottom: .7rem
+  .Header-credits
+    position: absolute
+    background-color: $bg-color-2
+    padding: .3rem
+    border-radius: 20px
+    bottom: 10px
+    right: 10px
+    p,
+    a
+      color: $bg-color
+      font-size: .7rem
 .Footer
   @include flex-center-center()
   background-color: $bg-color-3
