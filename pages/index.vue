6f8cba2c1c0d6c54ca6db2eadcefaf1564d51748
angelchavezinformatica <achavezg@unitru.edu.pe>
2024-06-18 19:15:04 -0500

    page- Index- header

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,38 @@
 <template>
-  <section>
-    <h1>Hola Mundo</h1>
-    <h2>Colaboradores</h2>
-    <ol>
-      <li v-for="collaborator in collaborators">
-        <a
-          :href="collaborator.github"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {{ collaborator.name }} ({{ collaborator.username }})
-        </a>
-      </li>
-    </ol>
-  </section>
+  <header class="Header">
+    <div class="Header-content">
+      <h1>Vivero La Huerta</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem
+        ut ex eos voluptate culpa ipsa sunt, porro autem non!
+      </p>
+    </div>
+  </header>
 </template>
 
-<script setup lang="ts">
-const collaborators = [
-  {
-    name: "Angel Chávez",
-    username: "angelchavezinformatica",
-    github: "https://github.com/angelchavezinformatica",
-  },
-  {
-    name: "Kevin Mostacero",
-    username: "Bhowa",
-    github: "https://github.com/Bhowa",
-  },
-  {
-    name: "Marlene Costa",
-    username: "dayanaraC",
-    github: "https://github.com/dayanaraC",
-  },
-  {
-    name: "Norma Rojas",
-    username: "KaterineDev",
-    github: "https://github.com/KaterineDev",
-  },
-  {
-    name: "Miguel Sánchez",
-    username: "SanchezIng",
-    github: "https://github.com/SanchezIng",
-  },
-];
-</script>
+<script setup lang="ts"></script>
 
-<style scope lang="sass"></style>
+<style scope lang="sass">
+.Header
+  @include flex-center-center()
+  background-color: $bg-color
+  background-image: url('/background.jpeg')
+  background-repeat: no-repeat
+  background-size: cover
+  background-position: center
+  width: 100%
+  height: 100vh
+  .Header-content
+    @include flex-center-center()
+    flex-direction: column
+    background-color: $bg-color-2
+    border-radius: 20px
+    width: calc( 100% - 2rem )
+    max-width: 700px
+    padding: .7rem .5rem
+    gap: .7rem
+    h1,
+    p
+      color: $bg-color
+      text-align: center
+</style>
